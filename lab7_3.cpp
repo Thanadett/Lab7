a12fcc0f74ca1ceea0bdc484c5dd9e9326799a43
Thanadett <tanapremin@gmail.com>
2023-12-08 16:45:10 +0700

    lab7_3.cpp completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,12 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-char before(char x){
-	//Write your function definition here
+char before(char x)
+{
+	if (x == 'A')
+	{
+		return 'Z';
+	}
+	if (x > 'A' && x <= 'Z')
+	{
+		return x - 1;
+	}
+	else
+	{
+		return 48;
+	}
 }
 
-int main(){
-	//Test Case
+int main()
+{
+	// Test Case
 	cout << before('A') << "\n";
 	cout << before('B') << "\n";
 	cout << before('P') << "\n";
